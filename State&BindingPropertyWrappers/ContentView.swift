db25b5cbf5cfe49286c4466c9b0fe8b3ddb7ea39
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-01-27 23:22:26 +0200

    State & Binding

--- State&BindingPropertyWrappers/ContentView.swift
+++ State&BindingPropertyWrappers/ContentView.swift
@@ -8,18 +8,53 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Modal: View {
+    @Binding var visibility: Bool
+    
     var body: some View {
-        VStack {
-            Text("0")
-                .font(.system(size: 40))
-            HStack {
-                Button("+") {
-                    print("Increase")
-                }.font(.system(size: 40))
-                Button("-") {
-                    print("Decrease")
-                }.font(.system(size: 40))
+        Rectangle()
+            .foregroundColor(.blue)
+            .edgesIgnoringSafeArea(.all)
+            .overlay (
+                VStack {
+                    Text("Your conter reached to 10")
+                        .padding()
+                    Button("Close") {
+                        self.visibility = false
+                    }.foregroundColor(.white)
+                }
+            )
+    }
+}
+
+struct ContentView: View {
+    @State private var counter: Int = 0
+    @State private var visible: Bool = false
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Text(String(self.counter))
+                    .font(.system(size: 40))
+                HStack {
+                    Button("+") {
+                        self.counter += 1
+                        if self.counter == 10 {
+                            self.visible = true
+                            self.counter = 0
+                        }
+                    }.font(.system(size: 40))
+                    Button("-") {
+                        if self.counter > 0 {
+                            self.counter -= 1
+                        }
+                    }.font(.system(size: 40))
+                }
+            }
+            if self.visible {
+                Modal(visibility: $visible)
+                    .animation(.easeOut)
+                    .offset(y: self.visible ? 0 : -10000)
             }
         }
     }
